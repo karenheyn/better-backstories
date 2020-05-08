46a23e506ea414fae5b35dd818341a617846aed9
karenheyn <karenheyn@gmail.com>
2020-05-08 14:54:58 -0400

    got description route working'

---
+++ src/components/Description.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>How It Works</h1>
+    <h2>
+      Put a little more
+      <span>character</span> in your character
+    </h2>
+    <img src="../assets/Characters.png" alt="characters" />
+    <p>
+      Better Backstories is a card-based random life history generator for
+      creative writing, educators, RPG Gamemasters, and even song writers. Works
+      with any role playing system.
+    </p>
+
+    <button>Learn More</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Description",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  font-family: "IM Fell English", serif;
+  color: #d2bf9e;
+  font-size: 6.5vw;
+  font-weight: 300;
+  text-align: left;
+  margin: 10px 65px;
+  text-shadow: 1px 1px 1px black;
+}
+</style>
---
+++ src/views/Product.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <Description msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Description from "@/components/Description.vue";
+
+export default {
+  name: "Product",
+  components: {
+    Description
+  }
+};
+</script>
