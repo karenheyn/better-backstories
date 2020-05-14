eb76470672183e1b5b59b9b507f21134f827b9d0
karenheyn <karenheyn@gmail.com>
2020-05-13 22:45:20 -0400

    added to about page

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -7,7 +7,28 @@
         <p>Better Backstories was designed by James ‘Jay’ Stilipec, Tim Mazurek, and Blazbaros. It was born out of a love of developing detailed life histories for role playing characters. It was brought to life through a Kickstarter campaign and the help of 1343 backers. While only asking for $3000, Jay was thrilled to garner more than $27,000 in pledges.</p>
       </div>
     </div>
-    <h1>Jay Stilipec’s Backstory</h1>
+    <div class="bio-wrapper">
+      <h1>Jay Stilipec’s Backstory</h1>
+      <div class="bio-container-right">
+        <div class="bio-inner-container-right">
+          <p>I love making role-playing characters. In order to make sure each character has a unique backstory of wonder and woe, I created Better Backstories. This easy to use and easy to customize system is genre neutral, and these cards can help everyone to make better backstories. It doesn't matter if you're a novice making their first fighter, or an expert game master developing their corrupted galactic president. Better Backstories are suitable for Player Characters, Non-Player Characters, and Game Masters alike. They are designed to jump start your imagination and make your characters come to life.</p>
+        </div>
+      </div>
+      <div class="bio-img-border">
+        <img class="bio-img" src="../assets/Jay.jpg" alt="Jay Stillipec" />
+      </div>
+    </div>
+    <div class="bio-wrapper">
+      <h1>Jay Stilipec’s Backstory</h1>
+      <div class="bio-container-left">
+        <div class="bio-inner-container-left">
+          <p>I love making role-playing characters. In order to make sure each character has a unique backstory of wonder and woe, I created Better Backstories. This easy to use and easy to customize system is genre neutral, and these cards can help everyone to make better backstories. It doesn't matter if you're a novice making their first fighter, or an expert game master developing their corrupted galactic president. Better Backstories are suitable for Player Characters, Non-Player Characters, and Game Masters alike. They are designed to jump start your imagination and make your characters come to life.</p>
+        </div>
+      </div>
+      <div class="bio-img-border">
+        <img class="bio-img" src="../assets/Jay.jpg" alt="Jay Stillipec" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -62,5 +83,48 @@ p {
   content: "";
   clear: both;
   display: table;
+}
+.bio-wrapper {
+  width: 90%;
+  margin: 70px auto 0 auto;
+  position: relative;
+}
+.bio-wrapper::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+.bio-container-right {
+  background: #d2bf9e;
+  width: 80%;
+  float: right;
+  padding: 20px 20px 20px 120px;
+}
+
+.bio-container-left {
+  background: #d2bf9e;
+  width: 80%;
+  float: left;
+  padding: 20px 20px 20px 120px;
+}
+
+.bio-inner-container-right {
+  background: #e3dcdc;
+  text-align: right;
+  padding: 25px 25px 25px 125px;
+}
+.bio-inner-container-left {
+  background: #e3dcdc;
+  text-align: right;
+  padding: 25px 25px 25px 125px;
+}
+.bio-img-border {
+  background: #1d5574;
+  padding: 20px;
+  position: absolute;
+  top: 30%;
+}
+.bio-img {
+  max-height: 32vw;
 }
 </style>
