27fc9fb62f6d335d7f5877f30ee252f3ee6d193e
karenheyn <karenheyn@gmail.com>
2020-05-14 00:54:56 -0400

    added shaneh bio

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -11,7 +11,7 @@
       <h1>Jay Stilipec’s Backstory</h1>
       <div class="bio-container-right">
         <div class="bio-inner-container-right">
-          <p>I love making role-playing characters. In order to make sure each character has a unique backstory of wonder and woe, I created Better Backstories. This easy to use and easy to customize system is genre neutral, and these cards can help everyone to make better backstories. It doesn't matter if you're a novice making their first fighter, or an expert game master developing their corrupted galactic president. Better Backstories are suitable for Player Characters, Non-Player Characters, and Game Masters alike. They are designed to jump start your imagination and make your characters come to life.</p>
+          <p>Jay has been playing video games and role-playing for more than 30 years. Originally from Alaska, they joined the U.S. Navy in 1995 to be a broadcast journalist. Jay assisted with the editing of a Mutants & Masterminds supplement by Brainstorm Studios called Hero Hooks in 2005. In 2013, they beta-tested D&D Next (D&D 5e) while deployed to Afghanistan. Jay retired in 2015 and now works as a public affairs instructor for the Department of Defense. They also cosplay in Washington D.C. under the pseudonym Jazmine Cosplays. Better Backstories is their first Kickstarter and first private business venture.</p>
         </div>
       </div>
       <div class="bio-img-border-left">
@@ -19,18 +19,41 @@
       </div>
     </div>
     <div class="bio-wrapper">
-      <h1>Jay Stilipec’s Backstory</h1>
+      <h1>Tim Mazurek’s Backstory</h1>
       <div class="bio-container-left">
         <div class="bio-inner-container-left">
-          <p>I love making role-playing characters. In order to make sure each character has a unique backstory of wonder and woe, I created Better Backstories. This easy to use and easy to customize system is genre neutral, and these cards can help everyone to make better backstories. It doesn't matter if you're a novice making their first fighter, or an expert game master developing their corrupted galactic president. Better Backstories are suitable for Player Characters, Non-Player Characters, and Game Masters alike. They are designed to jump start your imagination and make your characters come to life.</p>
+          <p>Tim has been teaching Graphic Design at colleges and universities all around the Baltimore beltway for the last 30 years. During the day, he leads a team of designers and photographers for the Department of Defense. At night, he runs a small design studio specializing in CD packaging design — A Little Fish, Graphic Design and Fine Typography. Tim is passionate and can be pretty geeky about design. Practicing graphic design is a fundamental part of his life.</p>
         </div>
       </div>
       <div class="bio-img-border-right">
-        <img class="bio-img" src="../assets/Jay.jpg" alt="Jay Stillipec" />
+        <img class="bio-img" src="../assets/Tim.png" alt="Tim Mazurek" />
+      </div>
+    </div>
+    <div class="bio-wrapper">
+      <h1>Blazbaros’ Backstory</h1>
+      <div class="bio-container-right">
+        <div class="bio-inner-container-right">
+          <p>Taylor Schmidt has been working as a freelance artist for the last 10 years, bringing life to a multitude of RPG characters under the name “Blazbaros” on the internet. He has a deep love for world building and roleplaying games, perhaps running too many games in his free time. He is currently working with Cybermoose Games creating Payload, a pen-and-paper RPG centered around giant mecha combat.</p>
+        </div>
+      </div>
+      <div class="bio-img-border-left">
+        <img class="bio-img blaz" src="../assets/Blazbaros.png" alt="Blazbaros" />
+      </div>
+    </div>
+    <div class="bio-wrapper">
+      <h1>Shaneh Woods</h1>
+      <div class="bio-container-left">
+        <div class="bio-inner-container-left">
+          <p>An accountant with the secret heart of a writer, long-time friend, and quote generator extraordinaire. Sarcasm, it's how she hugs!</p>
+        </div>
+      </div>
+      <div class="bio-img-border-right">
+        <img class="bio-img" src="../assets/Shaneh1.jpg" alt="Shaneh Woods" />
       </div>
     </div>
   </div>
 </template>
+
 
 <script>
 export default {
@@ -112,11 +135,19 @@ p {
   background: #e3dcdc;
   text-align: right;
   padding: 25px 25px 25px 125px;
+  min-height: 500px;
+  display: flex;
+}
+.bio-inner-container-right p,
+.bio-inner-container-left p {
+  align-self: center;
 }
 .bio-inner-container-left {
   background: #e3dcdc;
   text-align: left;
   padding: 25px 125px 25px 25px;
+  min-height: 500px;
+  display: flex;
 }
 .bio-img-border-left {
   background: #1d5574;
@@ -134,5 +165,8 @@ p {
 
 .bio-img {
   max-height: 32vw;
+}
+.blaz {
+  background: #ffffff;
 }
 </style>
