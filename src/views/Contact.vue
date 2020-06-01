6ecf8c1347881bf04c1fe6db5109d01f6f4e50c1
karenheyn <karenheyn@gmail.com>
2020-06-01 16:28:55 -0400

    added contact

---
+++ src/views/Contact.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="contact-email">
+    <a href="mailto: betterbackstories@gmail.com">
+      <i class="fa fa-envelope"></i>
+      Contact Us
+    </a>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+</script>
+<style scoped>
+a {
+  font-family: "Oswald", sans-serif;
+  color: white;
+  text-decoration: none;
+  font-size: 30px;
+  position: relative;
+  top: 100px;
+}
+
+i {
+  padding-bottom: 5px;
+  position: relative;
+  bottom: 1.5px;
+}
+</style>
