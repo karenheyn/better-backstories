2422559132568355bc4ddbd991d84a8a1ff1ffca
karenheyn <karenheyn@gmail.com>
2020-07-30 21:35:20 -0400

    finished switch, fixed typo in vueX store

--- src/views/Success.vue
+++ src/views/Success.vue
@@ -74,6 +74,48 @@ export default {
           console.log(error);
         });
     },
+    getBundleWithBox() {
+      axios("http://localhost:5000/pdf/bundlewithboxss", {
+        method: "GET",
+        responseType: "blob" //Force to receive data in a Blob Format
+      })
+        .then(response => {
+          const file = new Blob([response.data], { type: "application/pdf" });
+          const fileURL = URL.createObjectURL(file);
+          window.open(fileURL);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    getTechnicalBooster() {
+      axios("http://localhost:5000/pdf/technicalboosterss", {
+        method: "GET",
+        responseType: "blob" //Force to receive data in a Blob Format
+      })
+        .then(response => {
+          const file = new Blob([response.data], { type: "application/pdf" });
+          const fileURL = URL.createObjectURL(file);
+          window.open(fileURL);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    getMysticalBooster() {
+      axios("http://localhost:5000/pdf/mysticalboosterss", {
+        method: "GET",
+        responseType: "blob" //Force to receive data in a Blob Format
+      })
+        .then(response => {
+          const file = new Blob([response.data], { type: "application/pdf" });
+          const fileURL = URL.createObjectURL(file);
+          window.open(fileURL);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
 
     getPDF() {
       let product = window.localStorage.getItem("product");
@@ -85,10 +127,16 @@ export default {
           this.getCardBundle();
           break;
         case "Full Bundle with Box":
-          console.log("yay");
+          this.getBundleWithBox();
+          break;
+        case "Technical Booster":
+          this.getTechnicalBooster();
+          break;
+        case "Mystical Booster":
+          this.getMysticalBooster();
           break;
         default:
-          console.log("yay");
+          console.log("err, something went wrong....");
       }
     }
   }
