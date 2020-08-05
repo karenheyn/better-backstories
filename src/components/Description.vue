46fa0f3397f4ed27c8e384d7ef0f2532ac52f9a2
karenheyn <karenheyn@gmail.com>
2020-08-04 21:42:47 -0400

    fixed link and video

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -1,23 +1,6 @@
 <template>
   <div class="hello">
     <h1>How It Works</h1>
-    <img class="cards" src="../assets/Cards.png" alt="warrior" />
-
-    <img class="warrior" src="../assets/Tomas_Caraballo1.png" alt="warrior" />
-    <div class="outer-wrapper">
-      <div class="inner-wrapper">
-        <p>The cards can be used all together, or in smaller groups. Feel free to customize your deck by removing cards you don’t want to encounter.</p>
-        <p>When using with a role playing game, it is recommended you use the cards with your Gamemaster so they can help you incorporate your backstory into their plotlines.</p>
-        <p>
-          You can use the deck before or after creating some details of your character. Sometimes developing the backstory first can help with character development and decisions. It can also be fun applying a ‘musical prodigy’ element to
-          your Tiefling Barbarian.
-        </p>
-        <p>Shuffle the cards, then play a few face-up on a table. Five cards are a good place to start, but you can use as many or as few cards as you like.</p>
-        <p>Interpret the cards however you like. You can use the Title alone, developing your own interpretation of how it relates to your character’s backstory. Flavor Text provides some additional insight into the card, and Suggestions provide even more options. You can roll a d10 and compare to the chart, or play another card face down and use the number on the back. You may also pick a Suggestion you like or ignore them all and make up your own.</p>
-        <p>Use the details on the cards to develop your backstory. You can keep to the text as written, or use them as a guideline to jump start your imagination, and develop a unique story for your character.</p>
-      </div>
-    </div>
-    <img class="lucidra" src="../assets/Lucidra.png" alt="warrior" />
     <div class="iframe-outline">
       <div class="iframe-container">
         <iframe
@@ -31,6 +14,48 @@
         ></iframe>
       </div>
     </div>
+    <img class="cards" src="../assets/Cards.png" alt="warrior" />
+
+    <img class="warrior" src="../assets/Tomas_Caraballo1.png" alt="warrior" />
+    <div class="outer-wrapper">
+      <div class="inner-wrapper">
+        <p>
+          The cards can be used all together, or in smaller groups. Feel free to
+          customize your deck by removing cards you don’t want to encounter.
+        </p>
+        <p>
+          When using with a role playing game, it is recommended you use the
+          cards with your Gamemaster so they can help you incorporate your
+          backstory into their plotlines.
+        </p>
+        <p>
+          You can use the deck before or after creating some details of your
+          character. Sometimes developing the backstory first can help with
+          character development and decisions. It can also be fun applying a
+          ‘musical prodigy’ element to your Tiefling Barbarian.
+        </p>
+        <p>
+          Shuffle the cards, then play a few face-up on a table. Five cards are
+          a good place to start, but you can use as many or as few cards as you
+          like.
+        </p>
+        <p>
+          Interpret the cards however you like. You can use the Title alone,
+          developing your own interpretation of how it relates to your
+          character’s backstory. Flavor Text provides some additional insight
+          into the card, and Suggestions provide even more options. You can roll
+          a d10 and compare to the chart, or play another card face down and use
+          the number on the back. You may also pick a Suggestion you like or
+          ignore them all and make up your own.
+        </p>
+        <p>
+          Use the details on the cards to develop your backstory. You can keep
+          to the text as written, or use them as a guideline to jump start your
+          imagination, and develop a unique story for your character.
+        </p>
+      </div>
+    </div>
+    <img class="lucidra" src="../assets/Lucidra.png" alt="warrior" />
   </div>
 </template>
 
@@ -38,8 +63,8 @@
 export default {
   name: "Description",
   props: {
-    msg: String
-  }
+    msg: String,
+  },
 };
 </script>
 
@@ -50,9 +75,7 @@ h1 {
   font-size: 6.5vw;
   font-weight: 300;
   text-align: center;
-  margin: 10px 65px;
   text-shadow: 1px 1px 1px black;
-  margin: 0 0 30px 0;
 }
 .outer-wrapper {
   background: #d2bf9e;
